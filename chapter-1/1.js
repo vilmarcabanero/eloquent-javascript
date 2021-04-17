b575932d262147acf47bbb1c5715e278003a7327
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-04-18 02:30:45 +0800

    Chapter 1 done.

---
+++ chapter-1/1.js
@@ -0,0 +1,20 @@
+console.log(`Note that the character 'x' is a ${typeof 'x'}.`) //typeof
+console.log('Ab' < 'B')
+console.log(true ? 1 : 2)
+console.log(false ? 1 : 2) //ternary operator, conditional operator
+console.log(8 * null) //type coersion
+console.log('5' - 1)
+console.log('5' + 1)
+console.log('five' * 2)
+console.log(false == 0)
+
+console.log(null == undefined)
+console.log(null == 0)
+
+console.log('' === false)
+
+console.log(null || 'user')
+console.log('' || 'user')
+
+console.log(null && 'user')
+console.log('user' && false)
---
+++ chapter-2/1.js
@@ -0,0 +1,10 @@
+// //Bindings or variables
+// let vilsDebt = 1000
+// vilsDebt = vilsDebt - 400
+// console.log(vilsDebt)
+
+// let one = 1, two = 2
+// console.log(one + two)
+
+let theNumber = Number(prompt('Pick a number'))
+console.log('Your number is the square root of ' + theNumber * theNumber)
