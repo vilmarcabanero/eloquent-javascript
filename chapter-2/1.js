7734f1c1f3b0cc60c71262bdd2fe4c305ee8a2f1
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-04-18 18:55:55 +0800

    Added the else condition, that is, if the the input is not a valid number, alert it

--- chapter-2/1.js
+++ chapter-2/1.js
@@ -6,5 +6,13 @@
 // let one = 1, two = 2
 // console.log(one + two)
 
-let theNumber = Number(prompt('Pick a number'))
-console.log('Your number is the square root of ' + theNumber * theNumber)
+// let theNumber = Number(prompt('Pick a number'))
+// alert('Your number is the square root of ' + theNumber * theNumber)
+
+let theNumber = prompt('Pick a number')
+let theString = theNumber
+theNumber = Number(theNumber)
+
+if(!Number.isNaN(theNumber)) 
+  alert(`Your number is the square root of ${theNumber * theNumber}.`)
+else alert(`${theString} is not a valid number. Please try another value.`)
