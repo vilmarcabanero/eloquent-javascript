59746bb39fef40f5e8bb438a9f6b7c86cfc9ccd3
Vilmar Cabanero <vilmarcabanero@gmail.com>
2021-04-19 01:38:52 +0800

    Chapter 3

---
+++ chapter-3/1.js
@@ -0,0 +1,37 @@
+// const square = x => x * x
+
+// // console.log(square(12))
+
+// const makeNoise = () => {
+//   console.log('Pling!')
+// }
+
+// makeNoise()
+
+// const power = (base, exponent) => {
+//   let result = 1
+//   for (let i = 0; i < exponent; i++) {
+//     result *= base
+//   }
+//   return result
+// }
+
+// console.log(power(2, 10))
+
+// const hummus = factor => {
+// 	const ingredient = (amount, unit, name) => {
+// 		let ingredientAmount = amount * factor
+// 		if (ingredientAmount > 1) unit += 's'
+// 		console.log(`${ingredientAmount} ${unit} ${name}`)
+// 	}
+
+// 	ingredient(1, 'can', 'chickpeas')
+// 	ingredient(0.25, 'cup', 'tahini')
+// 	ingredient(0.25, 'cup', 'lemon juice')
+// 	ingredient(1, 'clove', 'garlic')
+// 	ingredient(2, 'tablespoon', 'olive oil')
+// 	ingredient(0.5, 'teaspoon', 'cumin')
+// }
+
+// hummus(5)
+
